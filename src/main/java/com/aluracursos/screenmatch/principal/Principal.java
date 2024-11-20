b2d7d91aaae81801f250f046b0139b6e5644fb97
Jeo98 <evaristoolivares.jeo@gmail.com>
2024-11-19 22:13:04 -0300

    ->buscarSerieWeb ->mostrarSeriesBuscadas ->enum Categoria

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -2,50 +2,71 @@ package com.aluracursos.screenmatch.principal;
 
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=TU-APIKEY-OMDB";
+    private final String API_KEY = "&apikey=8eb50e5";
     private ConvierteDatos conversor = new ConvierteDatos();
+    private List<DatosSerie> datosSeries = new ArrayList<>();
 
     public void muestraElMenu() {
+
+
         var opcion = -1;
-        while (opcion != 0) {
-            var menu = """
-                    1 - Buscar series 
-                    2 - Buscar episodios
-                    3 - Mostrar series buscadas
-                                  
-                    0 - Salir
-                    """;
-            System.out.println(menu);
-            opcion = teclado.nextInt();
-            teclado.nextLine();
 
-            switch (opcion) {
-                case 1:
-                    buscarSerieWeb();
-                    break;
-                case 2:
-                    buscarEpisodioPorSerie();
-                    break;
+            while (opcion != 0) {
+                try {
+                    var menu = """
+                            1 - Buscar series 
+                            2 - Buscar episodios
+                            3 - Mostrar series buscadas
+                            
+                            0 - Salir
+                            
+                            ->
+                            """;
+                    System.out.println(menu);
+                    opcion = teclado.nextInt();
+                    teclado.nextLine();
 
-                case 0:
-                    System.out.println("Cerrando la aplicación...");
-                    break;
-                default:
-                    System.out.println("Opción inválida");
-            }
-        }
+                    switch (opcion) {
+                        case 1:
+                            buscarSerieWeb();
+                            break;
+                        case 2:
+                            buscarEpisodioPorSerie();
+                            break;
+                        case 3:
+                            mostrarSeriesBuscadas();
+                            //datosSeries.forEach(System.out::println);
+                            break;
+
+                        case 0:
+                            System.out.println("Cerrando la aplicación...");
+                            break;
+                        default:
+                            System.out.println("Opción inválida");
+                    }
+                }catch (Exception e){
+                    System.out.println("error tipo de dato...");
+                }
+
+            } //endwhile
+
 
     }
+
+
 
     private DatosSerie getDatosSerie() {
         System.out.println("Escribe el nombre de la serie que deseas buscar");
@@ -67,10 +88,26 @@ public class Principal {
         temporadas.forEach(System.out::println);
     }
     private void buscarSerieWeb() {
-        DatosSerie datos = getDatosSerie();
+        DatosSerie datos = getDatosSerie(); //variable que almacena datos de la API tratados en getDatosSerie
+        datosSeries.add(datos); //guardo toda la lista de series en variable datos(que es lista tambien)...
         System.out.println(datos);
-    }
 
+
+    }
+    private void mostrarSeriesBuscadas() {
+
+            List<Serie> series =new ArrayList<>();
+
+            series = datosSeries.stream().map(s -> new Serie(s))
+                        .collect(Collectors.toList());
+
+            series.stream()
+                        .sorted(Comparator.comparing(Serie::getGenero))
+                        .forEach(System.out::println);
+
+
+
+    }
 
 }
 
