ceb824023253f5c34614f1c34665f8732e234522
Jeo98 <evaristoolivares.jeo@gmail.com>
2024-11-20 18:00:40 -0300

    ->Implementacion de CHATGPT API

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaCHATGPTAPI.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaCHATGPTAPI.java
@@ -7,7 +7,7 @@ public class ConsultaCHATGPTAPI {
 
     public static String obtenerTraduccion(String texto) {
         OpenAiService service = new OpenAiService("");
-        //sk-proj-3VkcDlOqSXJC8d2quOAYY1tubaTSwWSDYK5SMB JEO ze8R49Xhta3QrkfEMsu8hrVgHNhBB4KBWF5IT3BlbkFJVVaC_J-p-lEL4jgtjZ3L2DrXVBhD6H8OAbylG3jx2t_RqQdL2lHQOUR-7MXpRzA-m0LgHLnagA
+        //sk-proj-3VkcDlOqSXJC8d2quOAYY1tubaTSwWSDYK5SMB JEO98 ze8R49Xhta3QrkfEMsu8hrVgHNhBB4KBWF5IT3BlbkFJVVaC_J-p-lEL4jgtjZ3L2DrXVBhD6H8OAbylG3jx2t_RqQdL2lHQOUR-7MXpRzA-m0LgHLnagA
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
                 .prompt("traduce a español el siguiente texto: " + texto)
